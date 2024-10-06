8bd75906753cd998089abf8224782f55ed4f4404
Curio5813 <marcosglasner@gmail.com>
2024-10-06 09:05:55 +0100

    Reformatting website and debugging.

--- core/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def range(value):
-    return range(value)
--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'usuarios',
     'core',
-    'django_elasticsearch_dsl',
+    #'django_elasticsearch_dsl',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
