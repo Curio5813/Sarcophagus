157dc7559874b5f6dcaafab8fc75ab79b5efea58
Curio5813 <marcosglasner@gmail.com>
2025-02-12 20:52:45 +0000

    Editing settings.py to try to deploy my project Django using the DigitalOcean VM.

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
 from pathlib import Path
-
+from telnetlib import LOGOUT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yz$e2fxwy(wcpo+j^v54=s^&rnuqm(x(ell1=$d0eh4_nx8co2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['142.93.47.44']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
