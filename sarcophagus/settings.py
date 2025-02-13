d81b33472d15607ac391feb0b4b28f19c631d6de
Curio5813 <marcosglasner@gmail.com>
2025-02-13 21:15:35 +0000

    Changing allowed_hosts to try deploy Django app.

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yz$e2fxwy(wcpo+j^v54=s^&rnuqm(x(ell1=$d0eh4_nx8co2
 DEBUG = False
 
 # Prodction
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sarco-476c576b92e6.herokuapp.com', 'localhost']
 
 # Developing
 """
