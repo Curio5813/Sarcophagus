a1a96cc7d35cf96a939b5d971b1009a9a8577bb9
Curio5813 <marcosglasner@gmail.com>
2025-02-13 23:53:08 +0000

    Fixing some bugs in the deploy.

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'sarcophagus.urls'
 
 TEMPLATES = [
