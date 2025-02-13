0bf190f30c683a0f1290780c29ce655be13f381c
Curio5813 <marcosglasner@gmail.com>
2025-02-13 17:21:16 +0000

    Editing settings.py

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import urllib.parse
+
+senha_escapada = urllib.parse.quote('Curio5813#$')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,7 +90,7 @@ WSGI_APPLICATION = 'sarcophagus.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL', 'postgres://curio5813:Curio5813#$@localhost:5432/sarcophagus'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL', 'postgres://curio5813:{senha_escapada}@localhost:5432/sarcophagus'))
 }
 
 
