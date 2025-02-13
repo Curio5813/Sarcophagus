0722f7c9dc0df48947669fa4576c4eac2d5975ad
Curio5813 <marcosglasner@gmail.com>
2025-02-13 18:08:13 +0000

    Editing settings.py

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yz$e2fxwy(wcpo+j^v54=s^&rnuqm(x(ell1=$d0eh4_nx8co2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.heroku.com', 'localhost']
 
 # Application definition
 
@@ -90,14 +90,12 @@ import dj_database_url
 import os
 
 # Em Produção!
-"""
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-"""
 
 # Em desenvolvimento!
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -108,7 +106,7 @@ DATABASES = {
         'PORT': '5432',  # O padrão do PostgreSQL
     }
 }
-
+"""
 
 
 
@@ -158,7 +156,7 @@ AUTHENTICATION_BACKENDS = ['core.backends.EmailBackend',
                            'django.contrib.auth.backends.ModelBackend',]
 
 
-"""
+
 # Email produção
 
 EMAIL_HOST = 'localhost'
@@ -167,7 +165,6 @@ EMAIL_PORT = 587
 EMIAL_USE_TSL = True
 EMAIL_HOST_PASSWORD = 'curio5813#$'
 DEFAULT_FROM_EMAIL = 'contato@sarcophagus.com'
-"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -190,12 +187,12 @@ ELASTICSEARCH_DSL = {
 
 # Recursos de Segurança (ajustados para ambiente de desenvolvimento)
 SECURE_HSTS_SECONDS = 0
-SECURE_HSTS_SUBDOMAINS = False
-SECURE_CONTENT_TYPE_NOSNIFF = False
-SECURE_BROWSER_XSS_FILTER = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-CSRF_COOKIE_HTTPONLY = False
+SECURE_HSTS_SUBDOMAINS = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
 X_FRAME_OPTIONS = 'DENY'
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 
