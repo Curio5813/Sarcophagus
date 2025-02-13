635485a65c70e9847c8dd9281f52d227b679b58f
Curio5813 <marcosglasner@gmail.com>
2025-02-13 17:04:19 +0000

    Editing settings.py

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -96,6 +96,9 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
