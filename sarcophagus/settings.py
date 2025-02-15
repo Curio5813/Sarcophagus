3266e78beaf0551e7fbcb4bc7b011e7a706a0635
Curio5813 <marcosglasner@gmail.com>
2025-02-15 20:01:45 +0000

    Committing about little changes in settings.py

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-yz$e2fxwy(wcpo+j^v54=s^&rnuqm(x(ell1=$d0eh4_nx8co2
 DEBUG = True
 
 # Prodction and developing
-ALLOWED_HOSTS = ['sarco-476c576b92e6.herokuapp.com','sarcophagus.net', 'localhost']
+ALLOWED_HOSTS = ['sarco-476c576b92e6.herokuapp.com','sarcophagus.net', '*']
 
 # Application definition
 
