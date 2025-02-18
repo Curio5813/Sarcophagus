3be4d7ca6675f6d76f33fc986c35343b61a90e3b
Curio5813 <marcosglasner@gmail.com>
2025-02-18 05:49:09 +0000

    Ainda debugando erros de deploy.

--- sarcophagus/settings.py
+++ sarcophagus/settings.py
@@ -101,6 +101,9 @@ MEDIA_URL = f'{AWS_S3_CUSTOM_DOMAIN}/media/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'{AWS_S3_CUSTOM_DOMAIN}/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # **Configuração de Segurança**
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
