202554aa39cf7d41a84d54016846882188973484
Curio5813 <marcosglasner@gmail.com>
2025-02-28 15:56:29 +0000

    Alterando o arquivo adapters.py para que qualquer usuário possa fazer o login via API do google.

--- core/adapters.py
+++ core/adapters.py
@@ -4,9 +4,10 @@ from django.shortcuts import redirect
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
-        """Impede que o Allauth peça para completar o cadastro."""
-        return False  # Retorna False para impedir que a tela de cadastro apareça
+        """Permite que usuários sejam criados automaticamente"""
+        return True  # Permite novos cadastros
 
     def get_login_redirect_url(self, request):
         """Força o redirecionamento para a home após login"""
         return "/"
+
