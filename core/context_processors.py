438be51b7deb3a916776ca2cfe77c76e761eae60
Curio5813 <marcosglasner@gmail.com>
2025-03-07 18:03:48 +0000

    Alterando o 'context_processors.py' e o 'settings.py' para voltar a exibir feeds de notícias da Steam.

--- core/context_processors.py
+++ core/context_processors.py
@@ -15,22 +15,22 @@ def latest_comments(request):
     }
 
 
-def wpcg_rss_feed(request):
+def steam_rss_feed(request):
     """
     Obtém as últimas notícias de jogos do Rock Paper Shotgun e armazena em cache.
     """
-    feed_url = "worldofpcgames.com/feed"
+    feed_url = "https://store.steampowered.com/feeds/news.xml"
     cache_timeout = 1800  # Cache de 30 minutos
 
     # Verifica se já há notícias no cache
-    wpcg_news = cache.get("wpcg_rss_feed")
+    steam_news = cache.get("steam_rss_feed")
 
-    if not wpcg_news:
+    if not steam_news:
         feed = feedparser.parse(feed_url)
-        wpcg_news = []
+        steam_news = []
 
-        for entry in feed.entries[:6]:  # Pegando as 6 notícias mais recentes
-            wpcg_news.append({
+        for entry in feed.entries[:2]:  # Pegando as 6 notícias mais recentes
+            steam_news.append({
                 "title": entry.title,
                 "link": entry.link,
                 "published": entry.published,
@@ -38,6 +38,6 @@ def wpcg_rss_feed(request):
             })
 
         # Armazena no cache para evitar múltiplas requisições
-        cache.set("wpcg_rss_feed", wpcg_news, cache_timeout)
+        cache.set("steam_rss_feed", steam_news, cache_timeout)
 
-    return {"wpcg_news": wpcg_news}
+    return {"steam_news": steam_news}
