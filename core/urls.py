2d82fd8d4818143ce9585cd2dd652547c211f38e
Curio5813 <marcosglasner@gmail.com>
2025-04-11 19:28:17 +0100

    Alterando o nome da View para GameView e mudando a rota para games.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import (HomeView, DownloadView, CommunityView, ContactView,
+from .views import (HomeView, GameView, CommunityView, ContactView,
                     ReviewView, AvaliarJogoView, FavoritarJogoView,
                     GameSearchView, BlogView, TesteView, LoginView,
                     RegisterView, GameDetailView, MembroDetailView, BlogPostEditView,
@@ -20,7 +20,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('game-details/', GameSearchView.as_view(), name='game-details'),
     path('game/<int:id>/', GameDetailView.as_view(), name='game-details'),
-    path('download/', DownloadView.as_view(), name='download'),
+    path('games/', GameView.as_view(), name='games'),
     path('avaliar_jogo/', AvaliarJogoView.as_view(), name='avaliar_jogo'),
     path('favoritar_jogo/', FavoritarJogoView.as_view(), name='favoritar_jogo'),
     path('community/', CommunityView.as_view(), name='community'),
--- core/views.py
+++ core/views.py
@@ -76,11 +76,11 @@ class HomeView(TemplateView):
         return context
 
 
-class DownloadView(TemplateView):
+class GameView(TemplateView):
     template_name = 'download/download.html'
 
     def get_context_data(self, **kwargs):
-        context = super(DownloadView, self).get_context_data(**kwargs)
+        context = super(GameView, self).get_context_data(**kwargs)
         context['generos'] = Genero.objects.all()
         context['anos'] = range(1987, 2006)
 
